dfa1bf4b6d98dd4ce80746b9c2ae0121db85c189
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-23 15:56:14 -0500

    initial commit

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,32 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+function reset () {
+  $('#player1 td').removeClass('active').first().addClass('active');
+  $('#player2 td').removeClass('active').first().addClass('active');
+};
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+$(document).ready(function() {
+
+  $("body").keyup(event,function(){ 
+    if (event.keyCode == 81) {
+      var player1 = $('#player1 .active');
+      player1.next().addClass('active');
+      player1.removeClass('active');
+    }
+    else if (event.keyCode == 80) {
+      var player2 = $('#player2 .active');
+      player2.removeClass('active');
+      player2.next().addClass('active');
+    };
+
+    if ($('#player1 .active').size() == 0) {
+      alert("Player 1 Wins!!!!!");
+      reset();
+    }
+    else if ($('#player2 .active').size() == 0) {
+      alert("Player 2 Wins!!!!!");
+      reset();
+    };
+
+  });
+
+
 });
