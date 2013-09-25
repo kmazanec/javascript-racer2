336a4b78d79deea8d3def8c1f5cbd74e27311c6d
kmazanec <k.mazanec@gmail.com>
2013-09-24 19:03:54 -0500

    finished - hit the races

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -21,35 +21,51 @@ end
 
 # -----------POST--------------
 
-post '/login' do
-  @player1 = Racer.find_or_create_by(name: params[:player1_name])
-  @player2 = Racer.find_or_create_by(name: params[:player2_name])
-  @game = Game.create()
-  @game.racers << @player1
-  @game.racers << @player2
-  @game.save
-
-  if request.xhr?
-    content_type :json
-    { p1_name: @player1.name, p2_name: @player2.name, game_id: @game.id }.to_json
-  else
-    erb :index
-  end
-
-end
-
 
 post '/results' do
-  puts "Results:"
+  puts "Results: "
   puts params.inspect
 
-  game = Game.find_by(id: params[:game_id].to_i)
-  if game
-    game.update(winner_id: Racer.find_by(name: params[:winner]), time: params[:time].to_f)
-    content_type :json
-    { game_id: game.id }.to_json
-  else
-    redirect to ('/')
-  end
-
+  game = Game.create(winner_id: params[:winner], time: params[:time])
+  player1 = Racer.find_or_create_by(name: params[:player1_name])
+  player2 = Racer.find_or_create_by(name: params[:player2_name])
+  game.racers << player1
+  game.racers << player2
+  game.save
+  content_type :json
+  { game_id: game.id }.to_json
 end
+
+
+# post '/login' do
+#   @player1 = Racer.find_or_create_by(name: params[:player1_name])
+#   @player2 = Racer.find_or_create_by(name: params[:player2_name])
+#   @game = Game.create()
+#   @game.racers << @player1
+#   @game.racers << @player2
+#   @game.save
+
+#   if request.xhr?
+#     content_type :json
+#     { p1_name: @player1.name, p2_name: @player2.name, game_id: @game.id }.to_json
+#   else
+#     erb :index
+#   end
+
+# end
+
+
+# post '/results' do
+#   puts "Results:"
+#   puts params.inspect
+
+#   game = Game.find_by(id: params[:game_id].to_i)
+#   if game
+#     game.update(winner_id: Racer.find_by(name: params[:winner]), time: params[:time].to_f)
+#     content_type :json
+#     { game_id: game.id }.to_json
+#   else
+#     redirect to ('/')
+#   end
+
+# end
--- public/js/new_application.js
+++ public/js/new_application.js
@@ -10,12 +10,9 @@ Racer.prototype.move = function() {
 };
 
 
-function Game (url, data, racer1, racer2) {
+function Game (racer1, racer2) {
   this.racer1 = racer1;
   this.racer2 = racer2;
-  $.post(url, data, function(response){
-    this.id = response.game_id;
-  });
 }
 
 Game.prototype.render = function() {
@@ -26,27 +23,76 @@ Game.prototype.render = function() {
   $('#game_id').text(this.id);
 };
 
-Game.prototype.listen = function(event) {
-  console.log("Made it to the key listener");
-    if (event.keyCode == 81) {
-      this.racer1.move();
-    }
-    else if (event.keyCode == 80) {
-      this.racer2.move();
-    }
-
-    if (racer1.position.size() === 0) {
-      stop_game($('#p1').text());
-    }
-    else if (racer1.position.size() === 0) {
-      stop_game($('#p2').text());
-    }
+Game.prototype.render_results = function() {
+  $('#results_game_id').text(this.id);
+  $('#results_matchup').text(this.racer1.name + " vs. " + this.racer2.name);
+  $('#results_winner').text(this.winner);
+  $('#results_time').text(this.final_time);
+  $('#results_link').text("localhost:9393/game/"+this.id);
+  $('#results_link').attr("href", "/game/"+this.id);
+  $('#race_results').show();
 };
+
+Game.prototype.reset = function () {
+  $('#player1 td').removeClass('active').first().addClass('active');
+  $('#player2 td').removeClass('active').first().addClass('active');
+};
+
+// Game.prototype.listen = function(event) {
+//   console.log("Made it to the key listener");
+//   console.log(racer1);
+//   console.log(racer2);
+//     if (event.keyCode == 81) {
+//       this.racer1.move();
+//     }
+//     else if (event.keyCode == 80) {
+//       this.racer2.move();
+//     }
+//     if (racer1.position.size() === 0) {
+//       this.stop(this.racer1.name);
+//     }
+//     else if (racer1.position.size() === 0) {
+//       this.stop(this.racer2.name);
+//     }
+// };
 
 Game.prototype.start = function() {
   this.start_time = $.now();
-  $("body").bind("keyup",this.listen);
+  var game = this;
+  $("body").bind("keyup",function(event){
+    if (event.keyCode == 81) {
+      game.racer1.move();
+    }
+    else if (event.keyCode == 80) {
+      game.racer2.move();
+    }
+    if (game.racer1.position.size() === 0) {
+      game.stop(game.racer1.name);
+    }
+    else if (game.racer2.position.size() === 0) {
+      game.stop(game.racer2.name);
+    }
+  });
 };
+
+Game.prototype.end_time = function() {
+  this.final_time = (($.now() - this.start_time)/1000);
+};
+
+Game.prototype.stop = function(winner) {
+  this.end_time();
+  this.winner = winner;
+  game = this;
+  $("body").unbind("keyup",this.listen);
+  var url = '/results';
+  var data = { player1_name: this.racer1.name, player2_name: this.racer2.name, winner: winner, time: this.final_time };
+  $.post(url, data, function(response){
+    game.id = response.game_id;
+    game.render_results();
+  });
+};
+
+
 
 
 $(document).ready(function() {
@@ -55,19 +101,31 @@ $(document).ready(function() {
 
   $("#login_form").submit(function(event) {
     event.preventDefault();
-
     var racer1 = new Racer($('#login_form #player1_name').val() ,$('#player1 .active'));
     var racer2 = new Racer($('#login_form #player2_name').val() ,$('#player2 .active'));
-
-    game = new Game(url, data, racer1, racer2);
+    game = new Game(racer1, racer2);
     game.render();
   });
-
   $("#start_timer").on("click", function(event) {
     $("#elapsed_time").text("GO!");
-
     game.start();
   });
+  $("#race_again").on("click", function(event) {
+    game.reset();
+    $('#racetrack').hide();
+    $('#race_results').hide();
+    $("#elapsed_time").text("");
+    $('#login_container').show();
+  });
 
+  $("#exit").on("click", function(event) {
+    game.reset();
+    $('#racetrack').hide();
+    $('#race_results').hide();
+    $("#elapsed_time").text("");
+    $('#player1_name').val("");
+    $('#player2_name').val("");
+    $('#login_container').show();
+  });
 
 });
